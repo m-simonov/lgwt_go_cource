d2d793e07110c7a71254fa7772b449d17c5ad4ab
Maksim <simonov.maksim13@yandex.ru>
2024-03-06 21:44:28 +0300

    French greeting added and refactoring done

--- 01_hello_world/hello_test.go
+++ 01_hello_world/hello_test.go
@@ -4,14 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, world' when an empty sting is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, world!"
+		got := Hello("", "")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Bernard", "French")
+		want := "Bonjour, Bernard"
 		assertCorrectMessage(t, got, want)
 	})
 }
