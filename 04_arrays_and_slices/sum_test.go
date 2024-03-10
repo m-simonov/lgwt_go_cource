0bbc8ddfa40cf4752fbdb94f8ace9497e617914b
Maksim <simonov.maksim13@yandex.ru>
2024-03-10 18:26:05 +0300

    Added SumAll func and tests

--- 04_arrays_and_slices/sum_test.go
+++ 04_arrays_and_slices/sum_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"slices"
 	"testing"
 )
 
@@ -26,5 +27,20 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("sum 2 slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		// if !reflect.DeepEqual(got, want) {
+		// 	t.Errorf("got %v want %v", got, want)
+		// }
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+
 	})
 }
